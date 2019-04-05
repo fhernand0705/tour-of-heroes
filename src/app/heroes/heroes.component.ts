a9b9a200a5da3b8166fc8fa9fbfe3b1930c5860a
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-04-04 22:49:40 -0400

    added list of heroes to page + added click event to toggle data + styling

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,11 +7,12 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
   constructor() { }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  {id: 2, name: 'Arya'},
-  {id: 3, name: 'Jon'},
-  {id: 4, name: 'Sansa'},
-  {id: 5, name: 'Khaleesi'},
-  {id: 6, name: 'Tyrion'},
-  {id: 7, name: 'Sir Davvos'},
-  {id: 8, name: 'Brienne'},
-  {id: 9, name: 'Samwell'},
-  {id: 10, name: 'Brandon'}
+  {id: 11, name: 'Arya'},
+  {id: 12, name: 'Jon'},
+  {id: 13, name: 'Sansa'},
+  {id: 14, name: 'Khaleesi'},
+  {id: 15, name: 'Tyrion'},
+  {id: 16, name: 'Davvos'},
+  {id: 17, name: 'Brienne'},
+  {id: 18, name: 'Samwell'},
+  {id: 19, name: 'Brandon'},
+  {id: 20, name: 'Drogon'}
 ];
